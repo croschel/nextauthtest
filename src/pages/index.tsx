1a91c3baf4c15fde3d51fb0d4d142e51f616417f
Caique Roschel <croschel000@gmail.com>
2021-06-02 22:19:45 -0300

    creating new project with context auth

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { FormEvent, useState } from "react";
+
+export default function Home() {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
